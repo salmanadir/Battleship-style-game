a3d6360c76a7af965838944e5ca29b7eeabb060a
salmanadir <salmaazerty577@gmail.com>
2025-08-03 15:51:58 +0100

    adding the simple version so far

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Battleship-style-game
+
+I am gonna start by doing a simple version, so instead of a 2-D grid we hide the Startup in just a single row. And instead of three
+Startups, we use one and all that in command-line.
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,26 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleStartup theStartup = new SimpleStartup();
+
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theStartup.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while (isAlive) {
+            int guess = helper.getUserInput("enter a number");
+            String result = theStartup.checkYourself(guess);
+            numOfGuesses++;
+
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            }
+        }
     }
 }
