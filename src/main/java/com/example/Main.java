668cc35a7ae002bc5f2f1a4348d4f303056af94b
salmanadir <salmaazerty577@gmail.com>
2025-08-04 21:54:55 +0100

    change to arraylist

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         int numOfGuesses = 0;
@@ -7,17 +9,20 @@ public class Main {
 
         SimpleStartup theStartup = new SimpleStartup();
 
-        int randomNum = (int) (Math.random() * 5);
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        int randomNum = (int) (Math.random() * 5); // Génère un nombre entre 0 et 4
+        ArrayList<String> locations = new ArrayList<>();
+        locations.add(String.valueOf(randomNum));
+        locations.add(String.valueOf(randomNum + 1));
+        locations.add(String.valueOf(randomNum + 2));
         theStartup.setLocationCells(locations);
 
         boolean isAlive = true;
 
         while (isAlive) {
-            int guess = helper.getUserInput("enter a number");
+            String guess = String.valueOf(helper.getUserInput("enter a number"));
             String result = theStartup.checkYourself(guess);
             numOfGuesses++;
-
+            System.out.println(result);
             if (result.equals("kill")) {
                 isAlive = false;
                 System.out.println("You took " + numOfGuesses + " guesses");
--- src/main/java/com/example/SimpleStartup.java
+++ src/main/java/com/example/SimpleStartup.java
@@ -1,26 +1,25 @@
 package com.example;
 
-class SimpleStartup {
-    private int[] locationCells;
-    private int numOfHits = 0;
+import java.util.ArrayList;
 
-    public void setLocationCells(int[] locs) {
+public class SimpleStartup {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
     }
 
-    public String checkYourself(int guess) {
+    public String checkYourself(String userInput) {
         String result = "miss";
-        for (int cell : locationCells) {
-            if (guess == cell) {
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
             }
         }
-        if (numOfHits == locationCells.length) {
-            result = "kill";
-        }
-        System.out.println(result);
         return result;
     }
 }
